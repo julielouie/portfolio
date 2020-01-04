f2aad1d1ffeae1bf03ef0adf26debb47dcea5241
juliehchung <juliehchung96@gmail.com>
2020-01-04 02:19:27 +0000

    Adding hero to initial page

---
+++ client/components/hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Hero(props) {
+  return (
+    <div className="hero">
+      <div className="container col-md-8 h-100">
+        <div className="brand">JULIE H. CHUNG</div>
+        <div className="d-flex h-100">
+          <h1 className="hero-text align-self-center">
+            HERO TEXT HERE
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
