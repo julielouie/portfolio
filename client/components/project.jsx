ebe023454d0391282cecf572a018bfc7f942ba10
juliehchung <juliehchung96@gmail.com>
2020-01-07 19:23:10 +0000

    Styling text color and adding card animations

--- client/components/project.jsx
+++ client/components/project.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function Project(props) {
+  const textColor = props.current === 1 || props.current === 2 ? 'text-white' : null;
   return (
     <>
-      <h3>{props.projects[props.current].title}</h3>
-      <p>{props.projects[props.current].description}</p>
-      <div>{props.projects[props.current].link}</div>
+      <h3 className={textColor}>{props.projects[props.current].title}</h3>
+      <p className={textColor}>{props.projects[props.current].description}</p>
+      <div className={textColor}>{props.projects[props.current].link}</div>
     </>
   );
 
