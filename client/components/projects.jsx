ccc66b5bc6ed0cfd90079d9f0dbbe2af68c5a863
juliehchung <juliehchung96@gmail.com>
2020-01-07 02:00:09 +0000

    Created skeleton for individual project cards

--- client/components/projects.jsx
+++ client/components/projects.jsx
@@ -2,15 +2,36 @@ import React from 'react';
 
 function Projects(props) {
   return (
-    <div className="container">
-      <div>
+    <div className="projects container position-relative">
+      <div className="pb-5">
         <h1>PROJECTS</h1>
-        <div>Lorem ipsum about my projects</div>
+        <div>Full Stack Developer and some more lorem ipsum about my projects...</div>
+        <div className="">Github</div>
       </div>
-      <div className="container d-flex flex-row">
-        <div className="m-auto">Project 1</div>
-        <div className="m-auto">Project 2</div>
-        <div className="m-auto">Project 3</div>
+      <div className="row">
+        <div className="apps container d-flex flex-wrap justify-content-between position-absolute">
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title text-center my-auto py-3">BORKER</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title text-center my-auto py-3">Spontaneous.ly</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title text-center my-auto py-3">Bobafy</h5>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
