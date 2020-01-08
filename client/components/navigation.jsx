78e4ae44a01de3d4a952d02429fd1b951798a9b8
juliehchung <juliehchung96@gmail.com>
2020-01-08 03:16:55 +0000

    Sidenav icons will change to text on hover

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -4,25 +4,20 @@ class Navigation extends React.Component {
   render() {
     return (
       <div className="sidenav position-fixed">
-        <div className="d-flex flex-column align-items-center">
-          <i className="navicon home-icon fas fa-home mt-5"></i>
-          <div className="home m-4">Home</div>
+        <div className="click navicon d-flex flex-column align-items-center m-5">
+          <i className="home-icon fas fa-home"></i>
         </div>
-        <div className="d-flex flex-column align-items-center">
-          <i className="navicon me-icon fas fa-user"></i>
-          <div className="about-me m-4">About Me</div>
+        <div className="click navicon d-flex flex-column align-items-center m-5">
+          <i className="about-me-icon fas fa-user"></i>
         </div>
-        <div className="d-flex flex-column align-items-center">
-          <i className="navicon project-icon fas fa-project-diagram"></i>
-          <div className="project-list m-4">Projects</div>
+        <div className="click navicon d-flex flex-column align-items-center m-5">
+          <i className="project-list-icon fas fa-project-diagram"></i>
         </div>
-        <div className="d-flex flex-column align-items-center">
-          <i className="navicon contact-icon fas fa-address-book"></i>
-          <div className="contact m-4">Contact</div>
+        <div className="click navicon d-flex flex-column align-items-center m-5">
+          <i className="contact-icon fas fa-address-book"></i>
         </div>
-        <div className="d-flex flex-column align-items-center">
-          <i className="navicon resume-icon fas fa-file"></i>
-          <div className="resume m-4">Resume</div>
+        <div className="click navicon d-flex flex-column align-items-center m-5">
+          <i className="resume-icon fas fa-file"></i>
         </div>
       </div>
     );
