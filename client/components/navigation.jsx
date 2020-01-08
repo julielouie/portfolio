b98918a5cc7503e3b8f06826e4c845367896e3a6
juliehchung <juliehchung96@gmail.com>
2020-01-08 02:53:12 +0000

    Starting to make changes for navbar icon to text change on hover

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -3,21 +3,26 @@ import React from 'react';
 class Navigation extends React.Component {
   render() {
     return (
-      <div className="sidenav position-fixed d-flex flex-column align-items-center">
-        <div className="navicon home m-4">
-          <i className="fas fa-home"></i>
+      <div className="sidenav position-fixed">
+        <div className="d-flex flex-column align-items-center">
+          <i className="navicon home-icon fas fa-home mt-5"></i>
+          <div className="home m-4">Home</div>
         </div>
-        <div className="navicon about-me m-4">
-          <i className="fas fa-user"></i>
+        <div className="d-flex flex-column align-items-center">
+          <i className="navicon me-icon fas fa-user"></i>
+          <div className="about-me m-4">About Me</div>
         </div>
-        <div className="navicon project-list m-4">
-          <i className="fas fa-project-diagram"></i>
+        <div className="d-flex flex-column align-items-center">
+          <i className="navicon project-icon fas fa-project-diagram"></i>
+          <div className="project-list m-4">Projects</div>
         </div>
-        <div className="navicon contact m-4">
-          <i className="fas fa-address-book"></i>
+        <div className="d-flex flex-column align-items-center">
+          <i className="navicon contact-icon fas fa-address-book"></i>
+          <div className="contact m-4">Contact</div>
         </div>
-        <div className="navicon resume m-4">
-          <i className="fas fa-file"></i>
+        <div className="d-flex flex-column align-items-center">
+          <i className="navicon resume-icon fas fa-file"></i>
+          <div className="resume m-4">Resume</div>
         </div>
       </div>
     );
