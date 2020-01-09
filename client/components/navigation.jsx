9625fe2dab1df0273a717031efe65a32f82ae449
juliehchung <juliehchung96@gmail.com>
2020-01-09 18:51:56 +0000

    Added functionality for toggling navbar in mobile

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 
 class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
+    const show = this.state.isOpen ? 'show' : null;
     return (
       <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
         <div className="container">
           <a className="navbar-brand" href="#">Julie H. Chung</a>
-          <button className="collapsed navbar-toggler" type="button" data-toggle="collapse" data-target="#defaultNav" aria-controls="defaultNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="collapsed navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#defaultNav"
+            aria-controls="defaultNav"
+            aria-expanded={this.state.isOpen}
+            aria-label="Toggle navigation"
+            onClick={this.toggleNav}>
             <i className="fas fa-bars"></i>
           </button>
-          <div className="collapse navbar-collapse justify-content-end" id="defaultNav">
+          <div className={`collapse navbar-collapse justify-content-end ${show}`} id="defaultNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link" href="#home">Home</a>
