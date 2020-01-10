676f6e15fa5cef57334910a7f873c4b4661b2706
juliehchung <juliehchung96@gmail.com>
2020-01-10 01:18:34 +0000

    Completed basic styling for skills component

--- client/components/skills.jsx
+++ client/components/skills.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 
 function Skills(props) {
   return (
-    <div className="skills container d-flex flex-wrap" id="skills">
-      <div className="col-md-12">
-        <h1 className="text-center">Skills</h1>
-      </div>
-      <div className="row col-md-12 mx-auto">
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+    <div className="skills container d-flex flex-column justify-content-center" id="skills">
+      <div className="row mx-auto">
+        <h1 className="col-md-12 text-center py-5">Skills</h1>
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="devicon-javascript-plain colored"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="devicon-react-original colored"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="devicon-php-plain colored"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="mysql devicons devicons-mysql"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="jquery devicons devicons-jquery"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="devicon-bootstrap-plain colored"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="devicon-html5-plain colored"></i>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center">
+        <div className="icon col-6 col-md-3 d-flex justify-content-center p-3">
           <i className="devicon-css3-plain colored"></i>
         </div>
       </div>
