8cf7e1ed75d7edabecceba531d79d1ebee509ffa
juliehchung <juliehchung96@gmail.com>
2020-01-11 01:15:57 +0000

    Adding functionality for closing nav after clicking link in collapsed view

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -11,16 +11,21 @@ class Navigation extends React.Component {
   }
 
   toggleNav() {
-    this.setState({ isOpen: !this.state.isOpen });
+    if (window.innerWidth < 768) {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
   }
 
   render() {
-    const offset = -56;
     const show = this.state.isOpen ? 'show' : null;
+    let offsetNav = -56;
+    if (window.innerWidth < 768) {
+      offsetNav = -296;
+    }
     return (
       <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
         <div className="container">
-          <Link to="home" smooth={true} duration={100} offset={offset} className="click navbar-brand">Julie H. Chung</Link>
+          <Link to="home" smooth={true} duration={100} offset={offsetNav} className="click navbar-brand">Julie H. Chung</Link>
           <button className="collapsed navbar-toggler"
             type="button"
             data-toggle="collapse"
@@ -29,29 +34,27 @@ class Navigation extends React.Component {
             aria-expanded={this.state.isOpen}
             aria-label="Toggle navigation"
             onClick={() => {
+              this.toggleNav();
               scroll.scrollToTop();
-              if (this.state.isOpen) {
-                this.toggleNav();
-              }
             }}>
             <i className="fas fa-bars"></i>
           </button>
           <div className={`collapse navbar-collapse justify-content-end ${show}`} id="defaultNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link activeClass="active" to="about-me" spy={true} smooth={true} duration={100} offset={offset} className="click nav-link">About Me</Link>
+                <Link activeClass="active" to="about-me" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">About Me</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="projects" spy={true} smooth={true} duration={100} offset={offset} className="click nav-link">Projects</Link>
+                <Link activeClass="active" to="projects" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Projects</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="skills" spy={true} smooth={true} duration={100} offset={offset} className="click nav-link">Skills</Link>
+                <Link activeClass="active" to="skills" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Skills</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="tools" spy={true} smooth={true} duration={100} offset={offset} className="click nav-link">Tools</Link>
+                <Link activeClass="active" to="tools" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Tools</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="contact" spy={true} smooth={true} duration={100} offset={offset} className="click nav-link">Contact</Link>
+                <Link activeClass="active" to="contact" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Contact</Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="">Resume</a>
