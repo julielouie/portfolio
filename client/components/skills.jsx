fa0cbac182c611384d9cefe43444532f6fbcd8b6
juliehchung <juliehchung96@gmail.com>
2020-01-13 23:59:01 +0000

    Styling skills and tools icons, adding text names

--- client/components/skills.jsx
+++ client/components/skills.jsx
@@ -2,32 +2,40 @@ import React from 'react';
 
 function Skills(props) {
   return (
-    <div className="skills container d-flex flex-column justify-content-center" id="skills">
+    <div className="skills container d-flex flex-column justify-content-center py-5" id="skills">
       <div className="row mx-auto">
-        <h1 className="col-12 text-center pb-5">Skills</h1>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-javascript-plain colored"></i>
+        <h1 className="col-12 text-center pb-2">Skills</h1>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-javascript-plain colored"></i>
+          <p className="text-center">JavaScript</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-react-original colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-react-original colored"></i>
+          <p className="text-center">React.js</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-php-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-php-plain colored"></i>
+          <p className="text-center">PHP</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="mysql devicons devicons-mysql"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center mysql devicons devicons-mysql"></i>
+          <p className="text-center">MySQL</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="jquery devicons devicons-jquery"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center jquery devicons devicons-jquery"></i>
+          <p className="text-center">jQuery</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-bootstrap-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-bootstrap-plain colored"></i>
+          <p className="text-center">Bootstrap</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-html5-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-html5-plain colored"></i>
+          <p className="text-center">CSS3</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-css3-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-css3-plain colored"></i>
+          <p className="text-center">HTML5</p>
         </div>
       </div>
     </div>
--- client/components/tools.jsx
+++ client/components/tools.jsx
@@ -2,44 +2,56 @@ import React from 'react';
 
 function Tools(props) {
   return (
-    <div className="tools container d-flex flex-column justify-content-center" id="tools">
+    <div className="tools container d-flex flex-column justify-content-center py-5" id="tools">
       <div className="row mx-auto">
-        <h1 className="col-12 text-center pb-5">Tools</h1>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-amazonwebservices-original colored"></i>
+        <h1 className="col-12 text-center pb-2">Tools</h1>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-amazonwebservices-original colored"></i>
+          <p className="text-center">AWS</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-apache-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-apache-line colored"></i>
+          <p className="text-center">Apache</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-babel-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-babel-plain colored"></i>
+          <p className="text-center">BABEL</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-webpack-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-webpack-plain colored"></i>
+          <p className="text-center">Webpack</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicons devicons-npm"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicons devicons-npm"></i>
+          <p className="text-center">NPM</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-visualstudio-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-visualstudio-plain colored"></i>
+          <p className="text-center">VS Code</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="fab fa-slack"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center fab fa-slack"></i>
+          <p className="text-center">Slack</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-github-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-github-plain colored"></i>
+          <p className="text-center">Github</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-ubuntu-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-ubuntu-plain colored"></i>
+          <p className="text-center">Ubuntu</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="fab fa-figma"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center fab fa-figma"></i>
+          <p className="text-center">Figma</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-chrome-plain colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-chrome-plain colored"></i>
+          <p className="text-center">Chrome Dev Tools</p>
         </div>
-        <div className="icon col-6 col-md-3 d-flex justify-content-center p-4">
-          <i className="devicon-photoshop-line colored"></i>
+        <div className="col-6 col-md-3 d-flex flex-column p-4">
+          <i className="icon text-center devicon-photoshop-line colored"></i>
+          <p className="text-center">Adobe Photoshop</p>
         </div>
       </div>
     </div>
