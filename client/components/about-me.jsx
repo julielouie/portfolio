70484fbf8fb6fa36ee160deae990fbc49fff5f84
juliehchung <juliehchung96@gmail.com>
2020-01-13 21:16:25 +0000

    Fixed responsiveness of contact info

--- client/components/about-me.jsx
+++ client/components/about-me.jsx
@@ -11,42 +11,45 @@ function AboutMe(props) {
           </div>
           <div className="d-flex justify-content-center mb-5">
             <div className="m-auto">
-              <i className="fab fa-github"></i>
+              <i className="click link-icon fab fa-github"></i>
             </div>
             <div className="m-auto">
-              <i className="fab fa-linkedin-in"></i>
+              <i className="click link-icon fab fa-linkedin-in"></i>
             </div>
             <div className="m-auto">
-              <i className="fas fa-phone-alt"></i>
+              <i className="click link-icon fas fa-phone-alt"></i>
             </div>
             <div className="m-auto">
-              <i className="fas fa-envelope"></i>
+              <i className="click link-icon fas fa-envelope"></i>
             </div>
             <div className="m-auto">
-              <i className="fas fa-file"></i>
+              <i className="click link-icon fas fa-file"></i>
             </div>
           </div>
         </div>
         <div className="col-12 col-md-8">
           <p className="col-12">
             My name is Julie Chung, and I am a full stack developer with a passion and yearning to code, program, build, and learn more about new technologies and implementations.
-            My previous work experiences have been very human-centric. These took place in multiple, divergent environments, where I was able to pick up a variety of practical intrapersonal, technical, and communicative skills.
+            My previous work experiences have been very client-centric. Taking place in multiple, divergent environments, I was able to pick up a variety of practical intrapersonal, technical, and communicative skills.
+          </p>
+          <p className="col-12">
             From implementing ELAN behavioral coding software in a research laboratory, to drawing blood and taking radiographs of animals in a medical environment, I have had the privilege of learning how to mold myself into a multi-functional tool and asset.
           </p>
           <p className="col-12">
             While continuing to grow in these paths, it became very evident to me that there are many careers which, while fulfilling and deep in their own respects, often &quot;cap out&quot; in knowledge at a certain point in time.
-            I began to search backwards for an alternative career where I could always continue to learn and grow, and recalled a Java class that I took and enjoyed during university.
+            I began to search backwards for a career where I could always continue to learn and grow, and recalled a Java class that I took and enjoyed during university.
             After researching heavily for several months, I resigned from my position as a Veterinary Assistant and enrolled in the LearningFuze Accelerated Web Development Program in Irvine, CA.
           </p>
           <p className="col-12">
             At LearningFuze, I was provided with the opportunity to solidify my programming knowledge as a full-stack developer, while working on several projects supervised by multiple senior developers.
             As I continue to grow, I have developed a robust understanding of JavaScript, jQuery, and React. I have also enjoyed working with PHP and MySQL.
+          </p>
+          <p className="col-12">
             My client-focused experience combined with the skills and knowledge I have acquired as a Developer would make me a great asset to your team.
             It would be an amazing opportunity to be able to bring my enthusiasm and dedication for programming to your company.
           </p>
         </div>
       </div>
-      <div className="highlight"></div>
     </div>
   );
 }
--- client/components/contact.jsx
+++ client/components/contact.jsx
@@ -8,28 +8,28 @@ function Contact(props) {
       <div className="container m-auto">
         <h3 className="text-center py-4">JULIE H. CHUNG</h3>
         <div className="d-flex flex-wrap justify-content-center align-items-center pb-2">
-          <div className="px-4">
+          <div className="click col-12 col-md-4 text-center px-4">
             <i className="fas fa-map-marker-alt m-2"></i>
             Orange County/Los Angeles, CA
           </div>
-          <div className="px-4">
+          <div className="click col-12 col-md-4 text-center px-4">
             <i className="fas fa-phone-alt m-2"></i>
             (909) 957-8113
           </div>
-          <div className="px-4">
+          <div className="click col-12 col-md-4 text-center px-4">
             <i className="fas fa-envelope m-2"></i>
             juliehchung96@gmail.com
           </div>
         </div>
         <div className="row connect d-flex justify-content-center pb-4">
           <div className="m-3">
-            <i className="fab fa-github"></i>
+            <i className="click link-icon fab fa-github"></i>
           </div>
           <div className="m-3">
-            <i className="fab fa-linkedin-in"></i>
+            <i className="click link-icon fab fa-linkedin-in"></i>
           </div>
           <div className="m-3">
-            <i className="fas fa-file"></i>
+            <i className="click link-icon fas fa-file"></i>
           </div>
         </div>
         <hr />
