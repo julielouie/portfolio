0b4db7953f597b9d397081dfbc770e2d1ac5312d
juliehchung <juliehchung96@gmail.com>
2020-01-13 19:28:26 +0000

    Accounted for pixels of open nav when scrolling

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -25,7 +25,7 @@ class Navigation extends React.Component {
     return (
       <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
         <div className="container">
-          <Link to="home" smooth={true} duration={100} offset={offsetNav} className="click navbar-brand">Julie H. Chung</Link>
+          <Link to="home" spy={true} smooth={true} duration={100} offset={offsetNav} className="click navbar-brand" onClick={scroll.scrollToTop}>Julie H. Chung</Link>
           <button className="collapsed navbar-toggler"
             type="button"
             data-toggle="collapse"
@@ -33,28 +33,25 @@ class Navigation extends React.Component {
             aria-controls="defaultNav"
             aria-expanded={this.state.isOpen}
             aria-label="Toggle navigation"
-            onClick={() => {
-              this.toggleNav();
-              scroll.scrollToTop();
-            }}>
+            onClick={this.toggleNav}>
             <i className="fas fa-bars"></i>
           </button>
           <div className={`collapse navbar-collapse justify-content-end ${show}`} id="defaultNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link activeClass="active" to="about-me" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">About Me</Link>
+                <Link activeClass="active" to="about-me" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link" onClick={this.toggleNav}>About Me</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="projects" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Projects</Link>
+                <Link activeClass="active" to="projects" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link" onClick={this.toggleNav}>Projects</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="skills" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Skills</Link>
+                <Link activeClass="active" to="skills" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link" onClick={this.toggleNav}>Skills</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="tools" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Tools</Link>
+                <Link activeClass="active" to="tools" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link" onClick={this.toggleNav}>Tools</Link>
               </li>
               <li className="nav-item">
-                <Link activeClass="active" to="contact" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link">Contact</Link>
+                <Link activeClass="active" to="contact" spy={true} smooth={true} duration={100} offset={offsetNav} className="click nav-link" onClick={this.toggleNav}>Contact</Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="">Resume</a>
