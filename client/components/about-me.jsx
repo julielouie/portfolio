1c25928acd0fc6818fd7a7890e7ef476613f91f3
juliehchung <juliehchung96@gmail.com>
2020-01-13 19:47:07 +0000

    Organizing and editing about-me text

--- client/components/about-me.jsx
+++ client/components/about-me.jsx
@@ -4,8 +4,8 @@ function AboutMe(props) {
   return (
     <div className="container py-5" id="about-me">
       <div className="row my-5">
-        <div className="col-12 col-md-6">
-          <h4>About Me</h4>
+        <div className="col-12 col-md-4">
+          <h1>ABOUT ME</h1>
           <div className="my-5">
             <img src="/images/headshot_web.jpg" alt="profile" className="img-fluid" />
           </div>
@@ -27,19 +27,24 @@ function AboutMe(props) {
             </div>
           </div>
         </div>
-        <p className="col-12 col-md-6">
-            I am a full stack developer with a passion and yearning to code, program, build, and learn more about new technologies and implementations.
-            As a relatively recent university graduate, my previous work experience has been very human-centric.
-            They took place in multiple, divergent environments, where I was able to pick up a variety of practical intrapersonal, technical, and communicative skills.
+        <div className="col-12 col-md-8">
+          <p className="col-12">
+            My name is Julie Chung, and I am a full stack developer with a passion and yearning to code, program, build, and learn more about new technologies and implementations.
+            My previous work experiences have been very human-centric. These took place in multiple, divergent environments, where I was able to pick up a variety of practical intrapersonal, technical, and communicative skills.
             From implementing ELAN behavioral coding software in a research laboratory, to drawing blood and taking radiographs of animals in a medical environment, I have had the privilege of learning how to mold myself into a multi-functional tool and asset.
+          </p>
+          <p className="col-12">
             While continuing to grow in these paths, it became very evident to me that there are many careers which, while fulfilling and deep in their own respects, often &quot;cap out&quot; in knowledge at a certain point in time.
-            I began to search backwards for an alternative career where I could always continue to learn and grow, and recalled a Java class that I took and enjoyed in university.
-            After researching heavily for five months, I resigned from my position as a Veterinary Assistant and enrolled in the LearningFuze Accelerated Web Development Program in Irvine, CA.
+            I began to search backwards for an alternative career where I could always continue to learn and grow, and recalled a Java class that I took and enjoyed during university.
+            After researching heavily for several months, I resigned from my position as a Veterinary Assistant and enrolled in the LearningFuze Accelerated Web Development Program in Irvine, CA.
+          </p>
+          <p className="col-12">
             At LearningFuze, I was provided with the opportunity to solidify my programming knowledge as a full-stack developer, while working on several projects supervised by multiple senior developers.
-            As I continued to grow, I have developed a robust understanding of JavaScript, jQuery, and React. I have also enjoyed working with PHP and MySQL.
+            As I continue to grow, I have developed a robust understanding of JavaScript, jQuery, and React. I have also enjoyed working with PHP and MySQL.
             My client-focused experience combined with the skills and knowledge I have acquired as a Developer would make me a great asset to your team.
             It would be an amazing opportunity to be able to bring my enthusiasm and dedication for programming to your company.
-        </p>
+          </p>
+        </div>
       </div>
       <div className="highlight"></div>
     </div>
