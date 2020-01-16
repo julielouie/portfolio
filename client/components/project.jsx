a962e5a49cf3c5600cb0448bb9367ce04dfdc21a
juliehchung <juliehchung96@gmail.com>
2020-01-16 02:23:23 +0000

    Link styling for project links

--- client/components/project.jsx
+++ client/components/project.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 function Project(props) {
-  const textColor = props.current === 1 || props.current === 2 ? 'text-white' : null;
-  const liveCode = props.current !== 0 ? <a className={`${textColor} my-5 mr-3`} href={props.projects[props.current].gitLink}>
+  const textColor = props.current === 1 || props.current === 2 ? 'text-white' : 'text-dark';
+  const borderColor = props.current === 1 || props.current === 2 ? 'border' : 'border border-dark';
+  const liveCode = props.current !== 0 ? <a className={`proj-link ${textColor} my-5 mr-3 ${borderColor} rounded p-2`} href={props.projects[props.current].gitLink}>
     {props.projects[props.current].gitTitle }
     <i className="fas fa-external-link-alt ml-2"></i>
   </a> : null;
   return (
     <>
       <h3 className={`${textColor} mb-3 pt-3`}>{props.projects[props.current].title}</h3>
-      <h5 className={`${textColor} my-3`}>{props.projects[props.current].description}</h5>
+      <h5 className={`${textColor} my-3 py-2`}>{props.projects[props.current].description}</h5>
       {liveCode}
-      <a className={`${textColor} my-5`} href={props.projects[props.current].link}>
+      <a className={`proj-link ${textColor} my-5 ${borderColor} rounded p-2`} href={props.projects[props.current].link}>
         {props.projects[props.current].linkName}
         <i className="fas fa-external-link-alt ml-2"></i>
       </a>
