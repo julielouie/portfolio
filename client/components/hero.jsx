086d518f011fff3e6c4e339bc7cfb414c27a1473
juliehchung <juliehchung96@gmail.com>
2020-01-18 01:57:48 +0000

    Hero text style modifications

--- client/components/hero.jsx
+++ client/components/hero.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import TextLoop from 'react-text-loop';
 
 function Hero(props) {
-  const titleArray = ['Full Stack Developer', 'Creator', 'Artist', 'Inventor', 'Designer'];
+  const titleArray = ['Full Stack Developer', 'Creator', 'Artist', 'Adventurer', 'Designer', 'Innovator'];
   return (
     <div className="hero" id="home">
       <div className="container col-md-8">
         <div className="d-flex flex-column">
           <h1 className="name mt-5">JULIE H. CHUNG</h1>
-          <h4 className="title"><TextLoop interval={2000} springConfig={{ stiffness: 180, damping: 8 }}>{titleArray}</TextLoop></h4>
+          <h4 className="title"><TextLoop interval={2100} springConfig={{ stiffness: 180, damping: 8 }}>{titleArray}</TextLoop></h4>
         </div>
       </div>
     </div>
