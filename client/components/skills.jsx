80d4920fb71c00534aabcc827cb6ef2f88806295
juliehchung <juliehchung96@gmail.com>
2020-01-22 18:06:12 +0000

    Fixed another side scrolling bug, fixed skills and tools icon alignment

--- client/components/skills.jsx
+++ client/components/skills.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Skills(props) {
   return (
     <div className="d-flex flex-column justify-content-center py-5" id="skills">
-      <div className="mx-auto pt-5">
+      <div className="container mx-auto pt-5">
         <h1 className="heading col-12 text-center pb-3">SKILLS</h1>
-        <div className="d-flex flex-wrap pb-5">
+        <div className="row py-5">
           <div className="col-6 col-md-3 d-flex flex-column text-center p-4">
             <i className="icon devicon-javascript-plain colored"></i>
             <h5 className="m-3">JavaScript</h5>
