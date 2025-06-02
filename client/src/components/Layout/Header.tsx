69e9ef23f51ca1fa39055726151d340f88be577f
Julie Louie <juliehlouie@gmail.com>
2025-06-02 14:06:15 -0700

    feat: added handling for responsiveness

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -2,10 +2,11 @@ import { Dispatch, FC, SetStateAction } from 'react';
 import MuiAppBar from '@mui/material/AppBar';
 import { styled, useTheme } from '@mui/material/styles';
 import { PaletteMode, IconButton, Button, Typography, Box } from '@mui/material';
-import { DarkMode, LightMode } from '@mui/icons-material';
+import { DarkMode, LightMode, Menu } from '@mui/icons-material';
 import { UPDATE_THEME_MODE } from '../../constants/actions';
 import { useSession } from '../../contexts/SessionProvider';
 import { Link } from 'react-router-dom';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const StyledHeader = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -35,6 +36,8 @@ const Header: FC<HeaderProps> = (props) => {
 
   const { dispatch } = useSession();
 
+  const size = useWindowSize();
+
   const toggleTheme = () => {
     const newTheme = theme.palette.mode === 'light' ? 'dark' : 'light';
     dispatch({ type: UPDATE_THEME_MODE, payload: newTheme });
@@ -51,24 +54,32 @@ const Header: FC<HeaderProps> = (props) => {
             <Typography>Logo</Typography>
           </Box>
           <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-            <Button>
-              <Link to="#about" style={{ textDecoration: 'none', color: 'inherit' }}>
-                About
-              </Link>
-            </Button>
-            <Button>
-              <Link to="#experience" style={{ textDecoration: 'none', color: 'inherit' }}>
-                Experience
-              </Link>
-            </Button>
-            <Button>
-              <Link to="#contact" style={{ textDecoration: 'none', color: 'inherit' }}>
-                Contact
-              </Link>
-            </Button>
-            <IconButton onClick={toggleTheme} sx={{ ml: 2 }}>
-              {theme.palette.mode === 'light' ? <DarkMode /> : <LightMode />}
-            </IconButton>
+            {size.width < 640 ? (
+              <IconButton>
+                <Menu />
+              </IconButton>
+            ) : (
+              <>
+                <Button>
+                  <Link to="#about" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    About
+                  </Link>
+                </Button>
+                <Button>
+                  <Link to="#experience" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    Experience
+                  </Link>
+                </Button>
+                <Button>
+                  <Link to="#contact" style={{ textDecoration: 'none', color: 'inherit' }}>
+                    Contact
+                  </Link>
+                </Button>
+                <IconButton onClick={toggleTheme} sx={{ ml: 2 }}>
+                  {theme.palette.mode === 'light' ? <DarkMode /> : <LightMode />}
+                </IconButton>
+              </>
+            )}
           </Box>
         </Box>
       </Box>
