6cf0705f8f04475faad6cc6cccf40e191369f5da
Julie Louie <juliehlouie@gmail.com>
2025-06-05 11:28:07 -0700

    feat: wrote and sections general about me

--- client/src/components/About/index.tsx
+++ client/src/components/About/index.tsx
@@ -1,10 +1,42 @@
 import { FC } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 const About: FC = () => {
   return (
     <Grid id="about" sx={{ pt: 10, px: '1rem' }} container>
-      About
+      <Grid container sx={{ mx: 'auto', maxWidth: '80rem', width: '100%', py: '5rem' }}>
+        <Grid size={6} />
+        <Grid container flexDirection="column" justifyContent="center" spacing={2} size={6}>
+          <Grid>
+            <Typography variant="h3" fontWeight={400}>
+              About Me
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography variant="h4" fontWeight={400}>
+              Hi, I'm Julie, but most people call me Jules.
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography variant="h6" fontWeight={400}>
+              I'm a software engineer who loves building sleek, user-friendly apps with React, TypeScript, Node.js, and just the right
+              amount of ✨SaaS✨ (haha). I specialize in the frontend and move fast, write clean code, and live for those “this just works”
+              chef's kiss moments.
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography variant="h6" fontWeight={400}>
+              When I’m not coding, I’m painting tiny pokéballs on nails, hunting for my next food adventure, or losing track of time in a
+              cozy game (Witcher 3 counts as cozy, right?)
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography variant="h6" fontWeight={400}>
+              I’m all about good vibes, great design, and getting things done 💕
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
